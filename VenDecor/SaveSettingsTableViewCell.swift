ba87fc26a369f78c42c05eaceb2a6613ec26854c
Tin Vo <tinnvo1101@gmail.com>
2016-04-24 12:19:27 -0500

    Some small changes on profile picture

--- VenDecor/SaveSettingsTableViewCell.swift
+++ VenDecor/SaveSettingsTableViewCell.swift
@@ -27,21 +27,20 @@ class SaveSettingsTableViewCell: UITableViewCell {
     @IBAction func saveSettingsBtn(sender: AnyObject) {
         print( "saving settings" )
         
-        let usernameCell = self.settingsTableVC?.tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)) as! SettingsTableViewCell
-        let username = usernameCell.userInfoTxtField.text!
-        let emailCell = self.settingsTableVC?.tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 1)) as! SettingsTableViewCell
-        let email = emailCell.userInfoTxtField.text!
-        let zipcodeCell = self.settingsTableVC?.tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 2)) as! SettingsTableViewCell
-        let zipcode = zipcodeCell.userInfoTxtField.text!
+//        let usernameCell = self.settingsTableVC?.tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0)) as! ProfileTableViewCell
+//        let username = usernameCell.userInfoTxtField.text!
+//        let emailCell = self.settingsTableVC?.tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 1)) as! SettingsTableViewCell
+//        let email = emailCell.userInfoTxtField.text!
+//        let zipcodeCell = self.settingsTableVC?.tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 2)) as! SettingsTableViewCell
+//        let zipcode = zipcodeCell.userInfoTxtField.text!
+        
+        
+        
         
         //TODO: update DB
         // password saved somewhere?
        /* let user = ["email" : String(self.email.text!),"username": String(self.username.text!), "zipcode" : String(self.zipcode.text!) ]
         self.myRootRef.childByAppendingPath(uid).setValue(user)
         self.dismissViewControllerAnimated(true, completion: nil)*/
-        
-        
-        
     }
-    
 }
